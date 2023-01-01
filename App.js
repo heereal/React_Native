07b4011b108ea78630ba71f9b53b949ab80ecc5c
heeryoung <winterbooks@naver.com>
2023-01-01 20:33:24 +0900

    Implement firebase CRUD 모두 구현

--- App.js
+++ App.js
@@ -4,35 +4,57 @@ import { AntDesign } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 import { FontAwesome } from '@expo/vector-icons'; 
 import styled from '@emotion/native'
-import { useState } from 'react';
-
+import { useEffect, useState } from 'react';
+import { doc, getDoc, getDocs, addDoc, collection, query, documentId, updateDoc, deleteDoc } from 'firebase/firestore';
+import { db } from './firebaseConfig';
 
 const App = () => {
 
+  const [todos, setTodos] = useState([]);
   const [text, setText] = useState("");
   const [category, setCategory] = useState("Javascript");
-  const [todos, setTodos] = useState([]);
   const [editText, setEditText] = useState("");
-  
-  const neWTodo = {
-    id: Date.now(),
-    text,
-    isDone: false,
-    isEdit: false,
-    category
+
+  // todos가 변경될 때마다 데이터 새로 불러오기
+  useEffect(() => {
+    getTodos();
+  }, [todos]);
+ 
+  // firestore에서 데이터 가져오기
+  const getTodos = async () => {
+    const array = [];
+    const querySnapshot = await getDocs(collection(db, "todos"));
+    querySnapshot.forEach((doc) => array.push({docId: doc.id,...doc.data()}));
+    setTodos(array)
   };
 
+  // firestore에 데이터 추가하기
   const addTodo = async () => {
-    setTodos([...todos, neWTodo]);
+    const neWTodo = {
+      id: Date.now(),
+      text,
+      isDone: false,
+      isEdit: false,
+      category
+    };
+    await addDoc(collection(db, "todos"), neWTodo);
+    // setTodos([...todos, neWTodo]);
     setText("");
   };
 
-  const setDone = (id) => {
-    const copy = [...todos];
-    const newTodos = copy.map((todo) => todo.id === id ? {...todo, isDone: !todo.isDone} : todo);
-    setTodos(newTodos)
+  // [완료] 아이콘 클릭 시 작동-firestore 데이터 수정
+  const setDone = async (id) => {
+    const switchTodo = todos.find((todo) => todo.docId === id); 
+    const todoRef = doc(db, "todos", id);
+    await updateDoc(todoRef, {
+      isDone: !switchTodo.isDone
+    });
+    // const copy = [...todos];
+    // const newTodos = copy.map((todo) => todo.id === id ? {...todo, isDone: !todo.isDone} : todo);
+    // setTodos(newTodos)
   };
 
+  // [삭제] 아이콘 클릭 시 작동-firestore 데이터 삭제
   const deleteTodo = (id) => {
     Alert.alert("삭제", "정말 삭제하시겠습니다?", [
       {text: "취소", 
@@ -40,27 +62,42 @@ const App = () => {
       {text: "삭제", 
       style: "destructive", 
       onPress: () => {
-        const copy = [...todos];
-        const newTodos = copy.filter((todo) => todo.id !== id);
-        setTodos(newTodos)
+        deleteDoc(doc(db, "todos", id));
+        // const copy = [...todos];
+        // const newTodos = copy.filter((todo) => todo.id !== id);
+        // setTodos(newTodos)
       }}
     ])
   };
 
-  const setEdit = (id) => {
-    const copy = [...todos];
-    const newTodos = copy.map((todo) => todo.id === id ? {...todo, isEdit: !todo.isEdit} : todo);
-    setTodos(newTodos);
+  // [수정] 아이콘 클릭 시 input으로 변경
+  // firestore 데이터 수정
+  const setEdit = async (id) => {
+    const switchTodo = todos.find((todo) => todo.docId === id); 
+    const todoRef = doc(db, "todos", id);
+    await updateDoc(todoRef, {
+      isEdit: !switchTodo.isEdit
+    });
+    // const copy = [...todos];
+    // const newTodos = copy.map((todo) => todo.id === id ? {...todo, isEdit: !todo.isEdit} : todo);
+    // setTodos(newTodos);
   };
 
-  const editTodo = (id) => {
-    const copy = [...todos];
-    const newTodos = copy.map((todo) => todo.id === id ? {...todo, isEdit: false, text: editText} : todo )
-    setTodos(newTodos);
+  // [수정] input 제출 시 텍스트 수정됨
+  // firestore 데이터 수정
+  const editTodo = async (id) => {
+    const switchTodo = todos.find((todo) => todo.docId === id); 
+    const todoRef = doc(db, "todos", id);
+    await updateDoc(todoRef, {
+      text: editText,
+      isEdit: false
+    });
+    // const copy = [...todos];
+    // const newTodos = copy.map((todo) => todo.id === id ? {...todo, isEdit: false, text: editText} : todo )
+    // setTodos(newTodos);
   };
 
   return (
-    // <SafeAreaView style={styles.AndroidSafeArea}>
     <Wrap>
       <StatusBar style="auto" />
         <ButtonBox>
@@ -92,16 +129,16 @@ const App = () => {
               return (
               <OneToDo key={todo.id}>
                 { todo.isEdit 
-                ? <EditInput onChangeText={setEditText} onSubmitEditing={() => editTodo(todo.id)} /> 
+                ? <EditInput onChangeText={setEditText} onSubmitEditing={() => editTodo(todo.docId)} /> 
                 : <ToDoText style={{textDecorationLine: todo.isDone === false ? "none" : "line-through"}}>{todo.text}</ToDoText> }  
                 <IconBox>
-                  <TouchableOpacity onPress={() => setDone(todo.id)}>
+                  <TouchableOpacity onPress={() => setDone(todo.docId)}>
                     <AntDesign name="checksquare" size={26} color="black" style={{marginRight: 5}} />
                   </TouchableOpacity>
-                  <TouchableOpacity onPress={() => setEdit(todo.id)}>
+                  <TouchableOpacity onPress={() => setEdit(todo.docId)}>
                     <MaterialCommunityIcons name="pencil-circle-outline" size={26} color="black" style={{marginRight: 5}} />
                   </TouchableOpacity>
-                  <TouchableOpacity onPress={() => deleteTodo(todo.id)}>
+                  <TouchableOpacity onPress={() => deleteTodo(todo.docId)}>
                     <FontAwesome name="trash-o" size={26} color="black" />
                   </TouchableOpacity>
                 </IconBox>  
@@ -115,12 +152,6 @@ const App = () => {
   );
 };
 
-
-// const styles = StyleSheet.create({ 
-//   AndroidSafeArea: { 
-//     flex: 1, 
-//     backgroundColor: "white", 
-//     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0 } });
 
 const Wrap = styled.SafeAreaView`
   padding-top: 30px;
